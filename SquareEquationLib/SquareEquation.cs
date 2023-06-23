249a5df4ec29124b73841f20021fc3d797e24b35
RiverSpider <raevskih8888@mail.ru>
2023-06-23 19:24:12 +0600

    test

--- SquareEquationLib/SquareEquation.cs
+++ SquareEquationLib/SquareEquation.cs
@@ -8,7 +8,7 @@ public class SquareEquation
         double[] solve2 = new double[1];
         double[] solve3 = new double[2];
         double eps = 1e-9;
-        if (-eps < a < eps) throw new System.ArgumentException();
+        if (-eps < a && a < eps) throw new System.ArgumentException();
         if ( double.IsNaN(a) || double.IsInfinity(a) || double.IsNaN(b) || double.IsInfinity(b) || double.IsNaN(c) || double.IsInfinity(c)) throw new System.ArgumentException();
         b = b/a;
         c = c/a;
