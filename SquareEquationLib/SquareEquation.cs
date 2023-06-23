2f0e1d271b279220e47e682af4c55ee8616c8376
RiverSpider <raevskih8888@mail.ru>
2023-06-23 18:11:27 +0600

    123

--- SquareEquationLib/SquareEquation.cs
+++ SquareEquationLib/SquareEquation.cs
@@ -5,7 +5,8 @@ public class SquareEquation
     public static double[] Solve(double a, double b, double c)
     {
         if (a == 0) throw new System.ArgumentException();
-        if (c == Double.NaN || c == double.NegativeInfinity || c == double.PositiveInfinity || a == double.PositiveInfinity || b == double.PositiveInfinity || a == double.NegativeInfinity || b == double.NegativeInfinity || a == Double.NaN || b == Double.NaN) throw new ArgumentException();
+
+        if ( c == double.NegativeInfinity || c == Double.NaN || c == double.PositiveInfinity || a == double.PositiveInfinity || b == double.PositiveInfinity || a == double.NegativeInfinity || b == double.NegativeInfinity || a == Double.NaN || b == Double.NaN) throw new ArgumentException();
         double d = b*b-4*c*a;
         double[] solve = new double[2];
         if (d<-Single.Epsilon) return solve;
@@ -13,8 +14,8 @@ public class SquareEquation
             solve[0] = -(b + Math.Sign(b)* Math.Sqrt(d) )/2*a;
         } 
         else {
-            solve[0] = -(b + Math.Sign(b)* Math.Sqrt(d) )/2*a;
-            solve[1] = c/solve[0];
+            solve[0] = -( b + Math.Sign(b)* Math.Sqrt(d) )/2*a ;
+            solve[1] = c/solve[0]  ;
         }
         return solve;
     }
