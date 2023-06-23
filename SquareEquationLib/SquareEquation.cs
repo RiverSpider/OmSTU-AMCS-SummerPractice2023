de0e451bf06f5e888e5febdf6a02eefcc33e959f
RiverSpider <raevskih8888@mail.ru>
2023-06-23 18:59:26 +0600

    test

--- SquareEquationLib/SquareEquation.cs
+++ SquareEquationLib/SquareEquation.cs
@@ -7,12 +7,13 @@ public class SquareEquation
         double[] solve1 = {};
         double[] solve2 = new double[1];
         double[] solve3 = new double[2];
+        double eps = 1e-10;
         if (a == 0) throw new System.ArgumentException();
-        if ( c == double.NegativeInfinity || c == Double.NaN || c == double.PositiveInfinity || a == double.PositiveInfinity || b == double.PositiveInfinity || a == double.NegativeInfinity || b == double.NegativeInfinity || a == Double.NaN || b == Double.NaN) throw new System.ArgumentException();
+        if ( double.IsNaN(a) || double.IsInfinity(a) || double.IsNaN(b) || double.IsInfinity(b) || double.IsNaN(c) || double.IsInfinity(c)) throw new System.ArgumentException();
         double d = b*b-4*c*a;
         double[] solve = new double[2];
-        if (d<=-Single.Epsilon) return solve1;
-        else if (-Single.Epsilon < d && d < Single.Epsilon){
+        if (d<=-eps) return solve1;
+        else if (-eps < d && d < eps){
             solve2[0] = -(b)/2*a;
             return solve2;
         } 
